b9fa9f39a24fc7e05bc7fc065ccf09c9b22616c3
Masfiqun <masfiqunahmednooneknows@gmail.com>
2024-01-25 02:26:33 +0600

    buttons added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main(){
@@ -26,6 +28,39 @@ class Activity extends StatefulWidget {
 class _ActivityState extends State<Activity> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: 
+          Column(
+            children: [
+              SafeArea(
+                child: Text(" Electrical Machine Maintenance System",style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),),
+              ),
+              SizedBox(height: 20,),
+              Text("Location", style: TextStyle(fontWeight: FontWeight.bold),),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(onPressed: (){}, child: Text("Omor Ekushe Hall")),
+                  ElevatedButton(onPressed: (){}, child: Text("Muktijoddha Hall")),
+                ],
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: (){}, 
+                  child: Text("Main Pump"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange[600],
+                    foregroundColor: Colors.white
+                  )
+                  // ButtonStyle(
+                  //   backgroundColor: MaterialStatePropertyAll(Colors.orange[600]),
+                  //   foregroundColor: MaterialStatePropertyAll(Colors.white),
+                  // ),
+                )
+              )
+            ],
+          ),
+    );
   }
 }
