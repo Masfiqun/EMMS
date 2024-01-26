760433dca9a95525eccae13f42ee018b582fb5f8
Masfiqun <masfiqunahmednooneknows@gmail.com>
2024-01-26 14:51:07 +0600

    page adding

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main(){
+
+void main()  async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.landscapeLeft]
+  );
   runApp(const EMMS());
 }
 
@@ -14,6 +20,8 @@ class EMMS extends StatelessWidget {
     return MaterialApp(
       home: Activity(),
       debugShowCheckedModeBanner: false,
+      // theme: new ThemeData(scaffoldBackgroundColor: Colors.amber),
+
     );
   }
 }
@@ -28,44 +36,118 @@ class Activity extends StatefulWidget {
 class _ActivityState extends State<Activity> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Electical Machine Maintenance System", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),),
-      ),
-      body: 
-          Column(
-            children: [
-              SizedBox(height: 20,),
-              Text("Location", style: TextStyle(fontWeight: FontWeight.bold),),
-              SizedBox(height: 20,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(onPressed: (){}, child: Text("Omor Ekushe Hall")),
-                  ElevatedButton(onPressed: (){}, child: Text("Muktijoddha Hall")),
-                ],
-              ),
-              SizedBox(
-                width: 110,
-                height: 100,  
-                child: ElevatedButton(
+    return Container(
+      // decoration: BoxDecoration(
+      //   image: DecorationImage(
+      //     image: AssetImage('assets/images/973967.jpg'),
+      //     fit: BoxFit.cover,
+      //   )
+      // ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("Electical Machine Maintenance System", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),)),
+        ),
+
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
                   onPressed: (){}, 
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.volunteer_activism),
-                      Text("Pump")
-                    ],
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                    backgroundColor: Colors.orange[600],
-                    foregroundColor: Colors.white
-                  )
+                  child: Text("1st Phase Voltage")
                 ),
-              )
-            ],
-          ),
+
+                ElevatedButton(
+                  onPressed: (){}, 
+                  child: Text("2nd Phase Voltage")
+                ),
+
+                ElevatedButton(
+                  onPressed: (){}, 
+                  child: Text("3rd Phase Voltage")
+                ),
+
+                ElevatedButton(
+                  onPressed: (){}, 
+                  child: Text("Voltage Monitoring Graph")
+                ),
+              ],
+            ),
+
+            Text("2"),
+            
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: (){}, 
+                  child: Text("1st Phase Current")
+                ),
+
+                ElevatedButton(
+                  onPressed: (){}, 
+                  child: Text("2nd Phase Current")
+                ),
+
+                ElevatedButton(
+                  onPressed: (){}, 
+                  child: Text("3rd Phase Current")
+                ),
+
+                ElevatedButton(
+                  onPressed: (){}, 
+                  child: Text("Current Monitoring Graph")
+                ),
+              ],
+            )
+          ],
+          
+        )
+            
+      ),
     );
   }
 }
+
+
+
+              // Column(
+          //   children: [
+          //     SizedBox(height: 20,),
+          //     Text("Location", style: TextStyle(fontWeight: FontWeight.bold),),
+          //     SizedBox(height: 20,),
+          //     Row(
+          //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //       children: [
+          //         ElevatedButton(onPressed: (){}, child: Text("Omor Ekushe Hall")),
+          //         ElevatedButton(onPressed: (){}, child: Text("Muktijoddha Hall")),
+          //       ],
+          //     ),
+          //     SizedBox(
+          //       width: 110,
+          //       height: 100,  
+          //       child: ElevatedButton(
+          //         onPressed: (){}, 
+          //         child: Column(
+          //           mainAxisAlignment: MainAxisAlignment.center,
+          //           children: [
+          //             Icon(Icons.volunteer_activism),
+          //             Text("Pump")
+          //           ],
+          //         ),
+          //         style: ElevatedButton.styleFrom(
+          //           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          //           backgroundColor: Colors.orange[600],
+          //           foregroundColor: Colors.white
+          //         )
+          //       ),
+          //     )
+          //   ],
+          // ),
+
+
+
+
+         
